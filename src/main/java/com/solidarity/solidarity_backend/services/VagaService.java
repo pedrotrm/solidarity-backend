ecdbf4a6329706897279b4a4b0e86b860b0a917c
Pedro Thiago <pedrotrm@outlook.com.br>
2020-03-02 00:56:15 -0300

    implmentação do end-point vagas e vagas{id} parte 2

---
+++ src/main/java/com/solidarity/solidarity_backend/services/VagaService.java
@@ -0,0 +1,27 @@
+package com.solidarity.solidarity_backend.services;
+
+import com.solidarity.solidarity_backend.model.Vaga;
+import com.solidarity.solidarity_backend.repositories.VagaRepository;
+import com.solidarity.solidarity_backend.services.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VagaService {
+
+    @Autowired
+    private VagaRepository repository;
+
+    public List<Vaga> findAll() {
+        return repository.findAll();
+    }
+
+    public Vaga findById(Long id) {
+        Optional<Vaga> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+}
