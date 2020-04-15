241ed957c2d828a5ee3962bd009583f97f7daf19
Pedro Thiago <pedrotrm@outlook.com.br>
2020-04-15 12:49:41 -0300

    Revert "criado uma nova brach e corrigido problema dos endereços"
    This reverts commit a43422fdaf856f728caa146d0d5e26fd554bcc09.

--- src/main/java/com/solidarity/solidarity_backend/model/Entidade.java
+++ src/main/java/com/solidarity/solidarity_backend/model/Entidade.java
@@ -28,7 +28,7 @@ public class Entidade implements Serializable {
     private Integer causa2;
     private Integer numeroBeneficiarios;
 
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToOne(cascade = CascadeType.ALL)
     private Endereco endereco;
 
     @JsonBackReference
