a28e2ed1c82b11171d516226b04c0694d8cc3226
Pedro <pedrotrm@outlook.com.br>
2020-06-04 16:57:37 -0300

    Adicionado o EndPoint para criação de novos objetos do tipo experiencia.

--- src/main/java/com/solidarity/api/resources/MiniCurriculoResource.java
+++ src/main/java/com/solidarity/api/resources/MiniCurriculoResource.java
@@ -1,18 +1,20 @@
-package com.solidarity.solidaritybackend.resources;
+package com.solidarity.api.resources;
 
 
-import com.solidarity.solidaritybackend.dto.ExperienciaDTO;
-import com.solidarity.solidaritybackend.model.Experiencia;
-import com.solidarity.solidaritybackend.model.MiniCurriculo;
-import com.solidarity.solidaritybackend.services.MiniCurriculoService;
+import com.solidarity.api.dto.ExperienciaDTO;
+import com.solidarity.api.model.Experiencia;
+import com.solidarity.api.model.MiniCurriculo;
+import com.solidarity.api.services.MiniCurriculoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
-@RequestMapping(value = "voluntarios/{id}/minicurriculos")
+@RequestMapping(value = "voluntarios/minicurriculos/{id}")
 public class MiniCurriculoResource {
 
         @Autowired
@@ -22,6 +24,15 @@ public class MiniCurriculoResource {
         public ResponseEntity<MiniCurriculo> findById(@PathVariable("id") Long id){
             MiniCurriculo obj = miniCurriculoService.getById(id);
             return ResponseEntity.ok().body(obj);
+        }
+
+        @RequestMapping(value = "/experiencias", method = RequestMethod.POST)
+        public ResponseEntity<Void> insertExperiencia(@PathVariable("id") Long id,
+                                                      @Valid @RequestBody Experiencia experiencia){
+            miniCurriculoService.createExperiencia(experiencia, id);
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}").buildAndExpand(experiencia.getId()).toUri();
+            return ResponseEntity.created(uri).build();
         }
 
         @RequestMapping(value = "experiencias/{experienciaId}",method = RequestMethod.PUT)
