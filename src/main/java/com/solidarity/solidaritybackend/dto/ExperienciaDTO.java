87f3227c0de028613cf427c49b7baaf1db6c8621
Pedro <pedrotrm@outlook.com.br>
2020-06-04 14:39:31 -0300

    Implementado a estrutrura de DTO para atualização de Experiencia no MiniCurriculo e implmentado o servico para a atualização.

---
+++ src/main/java/com/solidarity/solidaritybackend/dto/ExperienciaDTO.java
@@ -0,0 +1,83 @@
+package com.solidarity.solidaritybackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.solidarity.solidaritybackend.model.Experiencia;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class ExperienciaDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    private String nomeEmpresa;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull(message = "Preenchimento obrigatório")
+    @PastOrPresent
+    private LocalDate dataEntrada;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @PastOrPresent
+    private LocalDate dataSaida;
+
+    private String atribuicoes;
+
+    public ExperienciaDTO() {
+    }
+
+    public ExperienciaDTO(Experiencia obj){
+        this.id = obj.getId();
+        this.nomeEmpresa = obj.getNomeEmpresa();
+        this.dataEntrada = obj.getDataEntrada();
+        this.dataSaida = obj.getDataSaida();
+        this.atribuicoes = obj.getAtribuicoes();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeEmpresa() {
+        return nomeEmpresa;
+    }
+
+    public void setNomeEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    public LocalDate getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(LocalDate dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public LocalDate getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(LocalDate dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public String getAtribuicoes() {
+        return atribuicoes;
+    }
+
+    public void setAtribuicoes(String atribuicoes) {
+        this.atribuicoes = atribuicoes;
+    }
+}
--- src/main/java/com/solidarity/solidaritybackend/services/MiniCurriculoService.java
+++ src/main/java/com/solidarity/solidaritybackend/services/MiniCurriculoService.java
@@ -1,6 +1,7 @@
 package com.solidarity.solidaritybackend.services;
 
 
+import com.solidarity.solidaritybackend.dto.ExperienciaDTO;
 import com.solidarity.solidaritybackend.model.Experiencia;
 import com.solidarity.solidaritybackend.model.MiniCurriculo;
 import com.solidarity.solidaritybackend.repositories.MiniCurriculoRepository;
@@ -8,6 +9,7 @@ import com.solidarity.solidaritybackend.services.exception.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
@@ -22,9 +24,22 @@ public class MiniCurriculoService {
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + MiniCurriculo.class.getName()));
     }
 
-    public Experiencia getExperienciaById(Long experienciaId, Long miniCurriculoId){
-        Optional<Experiencia> obj = repository.findByExperienciaIdAndMinicurriculoId(experienciaId, miniCurriculoId);
-        return obj.orElseThrow(() -> new ObjectNotFoundException(
-                "Objeto não encontrado! Id: " + experienciaId + ", Tipo: " + Experiencia.class.getName()));
+    @Transactional
+    public void updateExperiencia(Experiencia obj){
+        Experiencia newObj = repository.findByExperienciaId(obj.getId());
+        updateDataExperiencia(newObj, obj);
+        repository.updateExperiencia(newObj);
     }
+
+    public Experiencia fromExperienciaDTO(ExperienciaDTO objDto){
+        return new Experiencia(objDto.getId(), objDto.getNomeEmpresa(),objDto.getDataEntrada(),objDto.getDataSaida(),objDto.getAtribuicoes());
+    }
+
+    private void updateDataExperiencia(Experiencia newObj, Experiencia obj){
+        newObj.setNomeEmpresa(obj.getNomeEmpresa());
+        newObj.setDataEntrada(obj.getDataEntrada());
+        newObj.setDataSaida(obj.getDataSaida());
+        newObj.setAtribuicoes(obj.getAtribuicoes());
+    }
+
 }
