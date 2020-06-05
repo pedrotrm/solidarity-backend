1d6ccfa777a282308684b2d81cf28d696762d1a3
Pedro <pedrotrm@outlook.com.br>
2020-06-05 09:07:44 -0300

    Implementado o endpoint /voluntarios/minicurriculos/{id}/formacoes/{formacoesId} do verbo PUT para atuazação de formação.

--- src/main/java/com/solidarity/api/resources/MiniCurriculoResource.java
+++ src/main/java/com/solidarity/api/resources/MiniCurriculoResource.java
@@ -2,7 +2,9 @@ package com.solidarity.api.resources;
 
 
 import com.solidarity.api.dto.ExperienciaDTO;
+import com.solidarity.api.dto.FormacaoDTO;
 import com.solidarity.api.model.Experiencia;
+import com.solidarity.api.model.Formacao;
 import com.solidarity.api.model.MiniCurriculo;
 import com.solidarity.api.services.MiniCurriculoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +25,7 @@ public class MiniCurriculoResource {
         @GetMapping
         public ResponseEntity<MiniCurriculo> findById(@PathVariable("id") Long id){
             MiniCurriculo obj = miniCurriculoService.getById(id);
-            return ResponseEntity.ok().body(obj);
+                return ResponseEntity.ok().body(obj);
         }
 
         @RequestMapping(value = "/experiencias", method = RequestMethod.POST)
@@ -32,26 +34,33 @@ public class MiniCurriculoResource {
             miniCurriculoService.createExperiencia(experiencia, id);
             URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                     .path("/{id}").buildAndExpand(experiencia.getId()).toUri();
-            return ResponseEntity.created(uri).build();
+                return ResponseEntity.created(uri).build();
         }
 
 
-        @RequestMapping(value = "experiencias/{experienciaId}",method = RequestMethod.PUT)
+        @RequestMapping(value = "/experiencias/{experienciaId}",method = RequestMethod.PUT)
         public ResponseEntity<Void> updateExperiencia(@PathVariable("experienciaId") Long experienciaId,
                                                       @Valid @RequestBody ExperienciaDTO objDto){
 
             Experiencia obj = miniCurriculoService.fromExperienciaDTO(objDto);
             obj.setId(experienciaId);
             miniCurriculoService.updateExperiencia(obj);
-            return ResponseEntity.noContent().build();
+                return ResponseEntity.noContent().build();
         }
 
-        @RequestMapping(value = "experiencias/{experienciaId}", method = RequestMethod.DELETE)
+        @RequestMapping(value = "/experiencias/{experienciaId}", method = RequestMethod.DELETE)
         public ResponseEntity<Void> deleteExperiencia(@PathVariable("experienciaId") Long experienciaId){
-                miniCurriculoService.deleteExperiencia(experienciaId);
-            return ResponseEntity.noContent().build();
+            miniCurriculoService.deleteExperiencia(experienciaId);
+                return ResponseEntity.noContent().build();
         }
 
-
+        @RequestMapping(value = "/formacoes/{formacaoId}", method = RequestMethod.PUT)
+        public ResponseEntity<Void> updateFormacao(@PathVariable("formacaoId") Long formacaoId,
+                                                   @Valid @RequestBody FormacaoDTO objDto){
+            Formacao obj = miniCurriculoService.fromFormacaoDTO(objDto);
+            obj.setId(formacaoId);
+            miniCurriculoService.updateFormacao(obj);
+                return ResponseEntity.noContent().build();
+        }
 
 }
