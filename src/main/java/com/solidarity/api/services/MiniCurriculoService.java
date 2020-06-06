c61029abdd0bac074aaf197564788739ba5566e3
Pedro <pedrotrm@outlook.com.br>
2020-06-06 00:11:20 -0300

    Implementado os metedos para CRUD de Projetos e a sobrecarga do ProjetoDTO.

--- src/main/java/com/solidarity/api/services/MiniCurriculoService.java
+++ src/main/java/com/solidarity/api/services/MiniCurriculoService.java
@@ -3,9 +3,11 @@ package com.solidarity.api.services;
 
 import com.solidarity.api.dto.ExperienciaDTO;
 import com.solidarity.api.dto.FormacaoDTO;
+import com.solidarity.api.dto.ProjetoDTO;
 import com.solidarity.api.model.Experiencia;
 import com.solidarity.api.model.Formacao;
 import com.solidarity.api.model.MiniCurriculo;
+import com.solidarity.api.model.Projeto;
 import com.solidarity.api.repositories.MiniCurriculoRepository;
 import com.solidarity.api.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
@@ -55,7 +57,6 @@ public class MiniCurriculoService {
         repository.saveFormacao(obj);
     }
 
-
     @Transactional
     public void updateFormacao(Formacao obj){
         Formacao newObj = repository.findByFormacaoId(obj.getId());
@@ -69,12 +70,35 @@ public class MiniCurriculoService {
         repository.deleteFormacao(obj);
     }
 
+    @Transactional
+    public void createProjeto(Projeto obj,Long curriculoId){
+        obj.setId(null);
+        obj.setCurriculo(getById(curriculoId));
+        repository.saveProjeto(obj);
+    }
+
+    @Transactional
+    public void updateProjeto(Projeto obj){
+        Projeto newObj = repository.findByProjetoId(obj.getId());
+        updateDataProjeto(newObj, obj);
+        repository.updateProjeto(newObj);
+    }
+
+    public void deleteProjeto(Long id){
+        Projeto obj = repository.findByProjetoId(id);
+        repository.deleteProjeto(obj);
+    }
+
     public Experiencia fromExperienciaDTO(ExperienciaDTO objDto){
         return new Experiencia(objDto.getId(), objDto.getNomeEmpresa(),objDto.getDataEntrada(),objDto.getDataSaida(),objDto.getAtribuicoes());
     }
 
     public Formacao fromFormacaoDTO(FormacaoDTO objDto){
         return new Formacao(objDto.getId(), objDto.getNomeCurso(), objDto.getNomeInstituicao(), objDto.getDataInicio(), objDto.getDataFim());
+    }
+
+    public Projeto fromProjetoDTO(ProjetoDTO objDto){
+        return new Projeto(objDto.getId(), objDto.getNomeProjeto(), objDto.getDescricao());
     }
 
     private void updateDataExperiencia(Experiencia newObj, Experiencia obj){
@@ -89,5 +113,10 @@ public class MiniCurriculoService {
         newObj.setNomeInstituicao(obj.getNomeInstituicao());
         newObj.setDataInicio(obj.getDataInicio());
         newObj.setDataFim(obj.getDataFim());
+    }
+
+    private void updateDataProjeto(Projeto newObj, Projeto obj){
+        newObj.setNomeProjeto(obj.getNomeProjeto());
+        newObj.setDescricao(obj.getDescricao());
     }
 }
