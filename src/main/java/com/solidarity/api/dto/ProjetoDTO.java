61b7e78aa798903ac278a210d62c8448f222f18e
Pedro <pedrotrm@outlook.com.br>
2020-06-06 00:12:33 -0300

    Implementado classe modelo do DTO de Projeto.

---
+++ src/main/java/com/solidarity/api/dto/ProjetoDTO.java
@@ -0,0 +1,43 @@
+package com.solidarity.api.dto;
+
+import com.solidarity.api.model.Projeto;
+
+import java.io.Serializable;
+
+public class ProjetoDTO implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nomeProjeto;
+    private String descricao;
+
+    public ProjetoDTO(Projeto obj) {
+        this.nomeProjeto = obj.getNomeProjeto();
+        this.descricao = obj.getDescricao();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeProjeto() {
+        return nomeProjeto;
+    }
+
+    public void setNomeProjeto(String nomeProjeto) {
+        this.nomeProjeto = nomeProjeto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
