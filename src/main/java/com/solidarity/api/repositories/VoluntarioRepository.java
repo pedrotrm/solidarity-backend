6b3db10b4e5a2e7346865df7e5464776ea838cf2
Pedro <pedrotrm@outlook.com.br>
2020-06-14 12:54:36 -0300

    Atualizado o import

--- src/main/java/com/solidarity/api/repositories/VoluntarioRepository.java
+++ src/main/java/com/solidarity/api/repositories/VoluntarioRepository.java
@@ -1,5 +1,6 @@
 package com.solidarity.api.repositories;
 
+import com.solidarity.api.model.Vaga;
 import com.solidarity.api.model.Voluntario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,9 @@ import javax.transaction.Transactional;
 
 @Repository
 public interface VoluntarioRepository extends JpaRepository<Voluntario, Long> {
+
+
+
 
         @Transactional
         Voluntario findByEmail(String email);
