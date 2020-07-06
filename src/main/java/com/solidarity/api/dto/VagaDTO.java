d1c8302d10fc3b09d674bbf8c275b104e6a0a128
Pedro <pedrotrm@outlook.com.br>
2020-07-06 08:30:58 -0300

    Adcionado classe DTO

---
+++ src/main/java/com/solidarity/api/dto/VagaDTO.java
@@ -0,0 +1,11 @@
+package com.solidarity.api.dto;
+
+import java.io.Serializable;
+
+public class VagaDTO implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
+    private Long id;
+
+}
