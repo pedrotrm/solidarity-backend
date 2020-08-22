35644595cacea2f2a33a9c253b2e2c12c350e964
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2020-08-22 00:11:11 -0300

    Corrigido tratamento de exception findbyid para Vaga

--- src/main/java/com/solidarity/api/resources/VagaResource.java
+++ src/main/java/com/solidarity/api/resources/VagaResource.java
@@ -22,20 +22,18 @@ public class VagaResource {
     public VagaResource(VagaService service) {
         this.service = service;
     }
-
     @GetMapping
     public ResponseEntity<List<Vaga>> findAll(){
         List<Vaga> list = service.findAll();
         return  ResponseEntity.ok().body(list);
     }
 
-    // Corrigir o Tratamento de exeção desse metodo
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<Vaga> findById(@PathVariable Long id){
         Vaga obj =  service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
 
     @GetMapping(value = "/causas/{causaId}")
     public ResponseEntity<List<Vaga>> findByCausa(@PathVariable Integer causaId){
@@ -44,19 +42,19 @@ public class VagaResource {
     }
 
 
-    @GetMapping(value = "habilidades/{habilidadeId}")
+    @GetMapping(value = "/habilidades/{habilidadeId}")
     public ResponseEntity<List<Vaga>> findByHabilidade(@PathVariable Integer habilidadeId){
         List<Vaga> list = service.findByHabilidade(habilidadeId);
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "tipos/{tipoId}")
+    @GetMapping(value = "/tipos/{tipoId}")
     public ResponseEntity<List<Vaga>> findByTipo(@PathVariable Integer tipoId){
         List<Vaga> list = service.findByTipo(tipoId);
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "nomes/{nome}")
+    @GetMapping(value = "/nomes/{nome}")
     public ResponseEntity<List<Vaga>> findByNome(@PathVariable String nome){
         List<Vaga> list = service.findByNome(nome);
         return ResponseEntity.ok().body(list);
