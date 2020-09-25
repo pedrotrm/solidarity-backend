46cb0a6993fee00fc12731c4c99c3579aed025bf
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2020-09-25 12:27:05 -0300

    Implmentado ententidade permissão

---
+++ src/main/java/com/solidarity/api/model/Permissao.java
@@ -0,0 +1,52 @@
+package com.solidarity.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb13_permissao")
+public class Permissao {
+
+    @Id
+    @Column(name = "tb13_id")
+    private Long id;
+
+    @Column(name = "tb13_descricao")
+    private String descricao;
+
+    public Permissao() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Permissao permissao = (Permissao) o;
+
+        return id != null ? id.equals(permissao.id) : permissao.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
