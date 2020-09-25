1e4deb4c067af69953e94704f8bf7c354870cc64
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2020-09-25 12:25:39 -0300

    Atualizado os imports

--- src/main/java/com/solidarity/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/solidarity/api/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.solidarity.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
