4b00be3d1c832c1caef030a537c98aff44e8edc9
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2020-09-25 01:19:25 -0300

    Implementação do AutorizationServerConfig

---
+++ src/main/java/com/solidarity/api/security/AuthorizationServerConfig.java
@@ -0,0 +1,59 @@
+package com.solidarity.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthorizationServerConfig(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("react")
+                .secret("react")
+                .scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(20)
+                .refreshTokenValiditySeconds(3600 * 24);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)  {
+        endpoints
+                .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .reuseRefreshTokens(false)
+                .authenticationManager(authenticationManager);
+    }
+
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("solidarity");
+        return accessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+}
