a719d417beae34e9bba6b59a0de87b839592d7a2
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2020-09-27 11:03:52 -0300

    Implementado novo contrato.

--- src/main/java/com/solidarity/api/services/SmtpEmailService.java
+++ src/main/java/com/solidarity/api/services/SmtpEmailService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -13,10 +16,21 @@ public class SmtpEmailService extends AbstractEmailService {
     @Autowired
     private MailSender mailSender;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
     public void enviarEmail(SimpleMailMessage msg) {
         LOG.info("Simulando envio de email");
         mailSender.send(msg);
+        LOG.info(msg.toString());
+        LOG.info("Email enviado");
+    }
+
+    @Override
+    public void enviarEmailHtml(MimeMessage msg) {
+        LOG.info("Simulando envio de email");
+        javaMailSender.send(msg);
         LOG.info(msg.toString());
         LOG.info("Email enviado");
     }
