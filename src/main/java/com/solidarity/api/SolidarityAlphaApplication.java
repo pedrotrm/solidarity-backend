ace9022117b66337345c17669a2259470bdf2ec5
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2020-09-27 13:34:27 -0300

    Implementado metodo run

--- src/main/java/com/solidarity/api/SolidarityAlphaApplication.java
+++ src/main/java/com/solidarity/api/SolidarityAlphaApplication.java
@@ -1,13 +1,20 @@
 package com.solidarity.api;
 
+import com.solidarity.api.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SolidarityAlphaApplication {
+public class SolidarityAlphaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolidarityAlphaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 	}
 
 }
