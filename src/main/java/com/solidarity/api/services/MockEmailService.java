720b161d19810704ed709a7a2aae606e2f2b05f1
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2020-09-27 11:03:05 -0300

    Implementado novo contrato

--- src/main/java/com/solidarity/api/services/MockEmailService.java
+++ src/main/java/com/solidarity/api/services/MockEmailService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
 
 
 public class MockEmailService extends AbstractEmailService {
@@ -16,4 +17,12 @@ public class MockEmailService extends AbstractEmailService {
         LOG.info(msg.toString());
         LOG.info("Email enviado");
     }
+
+    @Override
+    public void enviarEmailHtml(MimeMessage msg) {
+        LOG.info("Simulando envio de email HTML ...");
+        LOG.info(msg.toString());
+        LOG.info("Email enviado");
+    }
+
 }
