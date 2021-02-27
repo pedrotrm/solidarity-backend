782dae211d63462de68bad8fc1424b45dcb1089f
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2021-02-27 01:28:26 -0300

    Inserindo estados e algumas cidades no banco de dados

---
+++ src/main/resources/db/migration/V3__insert_tabela_cidade.sql
@@ -0,0 +1,8 @@
+
+-- Inserindo algumas cidades de teste
+
+INSERT INTO tb06_cidade (tb06_nome, fktb06tb05_estado_id) VALUES
+('São Luís',10),
+('Pindaré-Mirim',10),
+('Chapadinha',10),
+('Santa-Inês',10);
