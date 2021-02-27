688f2d0d9d7d82031edf1d7a2b2ccd444d1615c5
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2021-02-27 18:55:13 -0300

    Implementação de construtor com tratamento para campos nulos

--- src/main/java/com/solidarity/api/dto/EntidadeDTO.java
+++ src/main/java/com/solidarity/api/dto/EntidadeDTO.java
@@ -1,12 +1,12 @@
 package com.solidarity.api.dto;
 
 import com.solidarity.api.model.Entidade;
+import com.solidarity.api.model.enums.Causa;
 import com.solidarity.api.services.validation.EntidadeUpdate;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CNPJ;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 
@@ -15,19 +15,18 @@ public class EntidadeDTO implements Serializable {
 
     private Long id;
 
-    @Length(min = 5 ,max = 120, message = "O tamanho deve ser entre 5 e 200 caracteres")
+    @Length(min = 5 ,max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
 
-    @NotEmpty(message = "Preenchimento obrigatório")
     @Email(message = "Email invalido")
     private String email;
 
     @CNPJ(message = "CNPJ invalido")
     private String cnpj;
 
-    private Integer causa1;
-    private Integer causa2;
-    private String fotoPeril;
+    private Causa causa1;
+    private Causa causa2;
+    private String fotoPerfil;
     private String descricao;
     private String logadouro;
     private String numero;
@@ -35,25 +34,46 @@ public class EntidadeDTO implements Serializable {
     private String bairro;
     private String cep;
     private Long cidadeId;
+    private String telefone1;
+    private String telefone2;
+    private String telefone3;
 
     public EntidadeDTO() {
     }
 
-    public EntidadeDTO(Entidade obj){
-        id = obj.getId();
-        nome = obj.getNome();
-        email = obj.getEmail();
-        cnpj = obj.getCnpj();
-        causa1 = obj.getCausa1().getCode();
-        causa2 = obj.getCausa2().getCode();
-        if(obj.getFotoPerfil() != null) fotoPeril = obj.getFotoPerfil();
-        descricao = obj.getDescricao();
-        logadouro = obj.getEndereco().getLogadouro();
-        numero = obj.getEndereco().getNumero();
-        complemento = obj.getEndereco().getComplemento();
-        bairro = obj.getEndereco().getBairro();
-        cep = obj.getEndereco().getCep();
-        cidadeId = obj.getEndereco().getCidade().getId();
+    /*
+       Contrutor para criar o DTO apartir de um Voluntario
+       tratando os valores nulos.
+    */
+    public EntidadeDTO(Entidade entidade) {
+        this.id = entidade.getId();
+        this.nome = entidade.getNome();
+        this.email = entidade.getEmail();
+        this.causa1 = entidade.getCausa1();
+        this.descricao = entidade.getDescricao();
+        if (entidade.getCnpj() != null)
+            this.cnpj = entidade.getCnpj();
+        if (entidade.getCausa2() != null)
+            this.causa2 = entidade.getCausa2();
+        if (entidade.getFotoPerfil() != null)
+            this.fotoPerfil = entidade.getFotoPerfil();
+        if (entidade.getEndereco().getLogadouro() != null)
+            this.logadouro = entidade.getEndereco().getLogadouro();
+        if (entidade.getEndereco().getNumero() != null)
+            this.numero = entidade.getEndereco().getNumero();
+        if (entidade.getEndereco().getComplemento() != null)
+            this.complemento = entidade.getEndereco().getComplemento();
+        if (entidade.getEndereco().getBairro() != null)
+            this.bairro = entidade.getEndereco().getBairro();
+        if (entidade.getEndereco().getCep() != null)
+            this.cep = entidade.getEndereco().getCep();
+        if (!entidade.getTelefones().isEmpty() && entidade.getTelefones() != null) {
+            entidade.getTelefones().stream()
+                    .findFirst()
+                    .ifPresent(this::setTelefone1);
+        }
+        if (entidade.getEndereco().getCidade() != null)
+            this.cidadeId = entidade.getEndereco().getCidade().getId();
     }
 
     public Long getId() {
@@ -88,28 +108,28 @@ public class EntidadeDTO implements Serializable {
         this.cnpj = cnpj;
     }
 
-    public Integer getCausa1() {
+    public Causa getCausa1() {
         return causa1;
     }
 
-    public void setCausa1(Integer causa1) {
+    public void setCausa1(Causa causa1) {
         this.causa1 = causa1;
     }
 
-    public Integer getCausa2() {
+    public Causa getCausa2() {
         return causa2;
     }
 
-    public void setCausa2(Integer causa2) {
+    public void setCausa2(Causa causa2) {
         this.causa2 = causa2;
     }
 
-    public String getFotoPeril() {
-        return fotoPeril;
+    public String getFotoPerfil() {
+        return fotoPerfil;
     }
 
-    public void setFotoPeril(String fotoPeril) {
-        this.fotoPeril = fotoPeril;
+    public void setFotoPerfil(String fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
     }
 
     public String getDescricao() {
@@ -158,6 +178,30 @@ public class EntidadeDTO implements Serializable {
 
     public void setCep(String cep) {
         this.cep = cep;
+    }
+
+    public String getTelefone1() {
+        return telefone1;
+    }
+
+    public void setTelefone1(String telefone1) {
+        this.telefone1 = telefone1;
+    }
+
+    public String getTelefone2() {
+        return telefone2;
+    }
+
+    public void setTelefone2(String telefone2) {
+        this.telefone2 = telefone2;
+    }
+
+    public String getTelefone3() {
+        return telefone3;
+    }
+
+    public void setTelefone3(String telefone3) {
+        this.telefone3 = telefone3;
     }
 
     public Long getCidadeId() {
