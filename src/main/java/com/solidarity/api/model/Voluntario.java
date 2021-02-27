4915d956bfb4cef09ef1008a64cd1fd931f7cdb7
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2021-02-27 08:01:51 -0300

    Alterado construdor com argumentos e feito tratamento para getCausa2 não chamar quando estiver nulo.

--- src/main/java/com/solidarity/api/model/Voluntario.java
+++ src/main/java/com/solidarity/api/model/Voluntario.java
@@ -57,12 +57,11 @@ public class Voluntario implements Serializable {
     public Voluntario() {
     }
 
-    public Voluntario(Long id, String nome,String email, Causa causa1, Causa causa2, Endereco endereco) {
+    public Voluntario(Long id, String nome,String email, Causa causa1,Endereco endereco) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.causa1 = causa1.getCode();
-        this.causa2 = causa2.getCode();
         this.endereco = endereco;
     }
 
@@ -109,7 +108,10 @@ public class Voluntario implements Serializable {
     }
 
     public Causa getCausa2() {
-        return Causa.valorDe(causa2);
+        if (causa2 != null)
+            return Causa.valorDe(causa2);
+        else
+            return null;
     }
 
     public void setCausa2 (Causa causa2) {
