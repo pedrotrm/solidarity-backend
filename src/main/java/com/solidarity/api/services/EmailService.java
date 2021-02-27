099a23534d8a76027ff30b4b26bb3d3f94cf4ce3
Pedro Thiago Maciel <pedrotrm@outlook.com.br>
2021-02-27 12:08:36 -0300

    Novos contratos.

--- src/main/java/com/solidarity/api/services/EmailService.java
+++ src/main/java/com/solidarity/api/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.solidarity.api.services;
 
+import com.solidarity.api.model.Entidade;
 import com.solidarity.api.model.Voluntario;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -9,10 +10,15 @@ public interface EmailService {
 
     void enviarEmailConfirmacaoVoluntario(Voluntario voluntario);
 
-    void enviarEmail(SimpleMailMessage msg);
-
     void enviarEmailConfirmacaoVoluntarioHtml(Voluntario voluntario);
 
+    void enviarEmailConfirmacaoEntidade(Entidade entidade);
+
+    void enviarEmailConfirmacaoEntidadeHtml(Entidade entidade);
+
+    void enviarEmail(SimpleMailMessage msg);
+
     void enviarEmailHtml(MimeMessage msg);
+
 
 }
